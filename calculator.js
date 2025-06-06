a0ecc7f6ba46e74a69f667974d8702285069d104
Wulver2 <ross.shayla21@gmail.com>
2025-06-06 09:36:58 -0400

    created css, html, and js files and created functions for addition, subtraction, multiplication, and divison with two params for each.

---
+++ calculator.js
@@ -0,0 +1,13 @@
+function add(num1, num2) {
+    //may need to add error checking for non-numbers
+    return num1 + num2;
+};
+function subtract(num1, num2) {
+    return num1 - num2;
+};
+function multiply(num1, num2) {
+    return num1 * num2;
+};
+function divide(num1, num2) {
+    return num1/num2;
+};
